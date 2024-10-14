8c6ec03c1f0e8874508559ad2a06aef6a3b39688
MarahYAlosh <102860295+MarahYAlosh@users.noreply.github.com>
2024-10-14 15:03:21 -0500

    Update index.js

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ const __dirname = path.dirname(__filename);console.log(__dirname);
 const app = express();
 
 app.use(morgan("tiny"));app.use(
-  cors({    origin: ["http://localhost:3000", "https://mernstackquiz-8.onrender.com"],
+  cors({    origin: ["http://localhost:3000", "https://mern-stack-quiz-api2.vercel.app"],
     methods: ["GET", "POST"],    credentials: true, // للسماح بإرسال الكوكيز
   }));
 app.use(express.json());app.use(cookieParser());
